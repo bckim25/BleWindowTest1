7a8134a1e551d810e7b55797b7265d8810f2c12e
peppi <peppi@knou.ac.kr>
2021-09-29 18:50:11 +0900

    Add project files.

---
+++ BleWindowTest1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace BleWindowTest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbClient = new System.Windows.Forms.RadioButton();
+            this.rbServer = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lb
+            // 
+            this.lb.FormattingEnabled = true;
+            this.lb.ItemHeight = 15;
+            this.lb.Location = new System.Drawing.Point(12, 12);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(569, 259);
+            this.lb.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbClient);
+            this.groupBox1.Controls.Add(this.rbServer);
+            this.groupBox1.Location = new System.Drawing.Point(622, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(316, 108);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "type";
+            // 
+            // rbClient
+            // 
+            this.rbClient.AutoSize = true;
+            this.rbClient.Location = new System.Drawing.Point(33, 33);
+            this.rbClient.Name = "rbClient";
+            this.rbClient.Size = new System.Drawing.Size(62, 19);
+            this.rbClient.TabIndex = 2;
+            this.rbClient.TabStop = true;
+            this.rbClient.Text = "client";
+            this.rbClient.UseVisualStyleBackColor = true;
+            // 
+            // rbServer
+            // 
+            this.rbServer.AutoSize = true;
+            this.rbServer.Location = new System.Drawing.Point(33, 72);
+            this.rbServer.Name = "rbServer";
+            this.rbServer.Size = new System.Drawing.Size(68, 19);
+            this.rbServer.TabIndex = 2;
+            this.rbServer.TabStop = true;
+            this.rbServer.Text = "server";
+            this.rbServer.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(778, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "connect";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 310);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(566, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(622, 147);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(315, 64);
+            this.listBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 365);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lb;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton rbClient;
+        private System.Windows.Forms.RadioButton rbServer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
